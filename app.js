1fb52f3304428e94065ab168e45b7da0c43d9caf
tushar <tusharkhandelwal315@gmail.com>
2022-07-03 22:29:26 +0530

    changes done

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
 const db = mysql.createConnection({
@@ -40,94 +40,94 @@ app.get('/', (req, res) => {
     res.send("Hello");
 });
 
-app.get('/Loginask', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'templates', 'loginask.html'));
-});
+// app.get('/Loginask', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'templates', 'loginask.html'));
+// });
 
-app.get('/Signupask', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'templates', 'signupask.html'));
-});
+// app.get('/Signupask', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'templates', 'signupask.html'));
+// });
 
-app.get('/Login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'templates', 'login.html'));
-});
+// app.get('/Login', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'templates', 'login.html'));
+// });
 
-app.get('/Signup', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'templates', 'signup.html'));
-});
+// app.get('/Signup', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'templates', 'signup.html'));
+// });
 
-app.get('/Customerlogin', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'templates', 'customerlogin.html'));
-});
+// app.get('/Customerlogin', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'templates', 'customerlogin.html'));
+// });
 
-app.get('/uphaar', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'templates', 'signup.html'));
-});
+// app.get('/uphaar', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'templates', 'signup.html'));
+// });
 
-app.get('/Farmersell', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'templates', 'signup.html'));
-});
+// app.get('/Farmersell', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'templates', 'signup.html'));
+// });
 
-app.get('/Customerprofile', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'templates', 'signup.html'));
-});
+// app.get('/Customerprofile', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'templates', 'signup.html'));
+// });
 
-app.post('/Checksignup', (req, res) => {
+// app.post('/Checksignup', (req, res) => {
 
-    const number = req.body.number;
-    const password = req.body.password;
+//     const number = req.body.number;
+//     const password = req.body.password;
 
-    bcrypt.hash(password, saltRounds, (err, hash) => {
-        if (err) {
-            console.log(err);
-        }
-        db.query(
-            "INSERT INTO farmer (number, password) VALUES (?,?)",
-            [number, hash],
-            (err, result) => {
-                console.log(err);
-            }
-        );
-    })
+//     bcrypt.hash(password, saltRounds, (err, hash) => {
+//         if (err) {
+//             console.log(err);
+//         }
+//         db.query(
+//             "INSERT INTO farmer (number, password) VALUES (?,?)",
+//             [number, hash],
+//             (err, result) => {
+//                 console.log(err);
+//             }
+//         );
+//     })
 
-    // if(account already exits)
-    // Store it into database using bcrypt
-});
+//     // if(account already exits)
+//     // Store it into database using bcrypt
+// });
 
-app.post('/Checklogin', (req, res) => {
+// app.post('/Checklogin', (req, res) => {
 
-    const number = req.body.number;
-    const password = req.body.password;
+//     const number = req.body.number;
+//     const password = req.body.password;
 
-    db.query(
-        "SELECT * FROM farmer WHERE number = ?;",
-        number,
-        (err, result) => {
-            if (err) res.send({ err: err });
-            if (result.length > 0) {
-                bcrypt.compare(password, result[0].password, (error, response) => {
-                    if (response) {
-                        req.session.user = result;
-                        console.log(req.session.user);
-                        res.sendFile(path.join(__dirname, 'public', 'templates', 'Homefarmer.html'));
-                    } else {
-                        res.send({ message: "Wrong phone number/password combination!" });
-                    }
-                })
-            } else {
-                res.send({ message: "User does't exist" });
-            }
-        }
-    );
+//     db.query(
+//         "SELECT * FROM farmer WHERE number = ?;",
+//         number,
+//         (err, result) => {
+//             if (err) res.send({ err: err });
+//             if (result.length > 0) {
+//                 bcrypt.compare(password, result[0].password, (error, response) => {
+//                     if (response) {
+//                         req.session.user = result;
+//                         console.log(req.session.user);
+//                         res.sendFile(path.join(__dirname, 'public', 'templates', 'Homefarmer.html'));
+//                     } else {
+//                         res.send({ message: "Wrong phone number/password combination!" });
+//                     }
+//                 })
+//             } else {
+//                 res.send({ message: "User does't exist" });
+//             }
+//         }
+//     );
 
-});
+// });
 
-app.get('/homefarmer', (req, res) => {
-    if (req.session.user) {
-        res.sendFile(path.join(__dirname, 'public', 'templates', 'Homefarmer.html'));
-    }
-    else res.sendFile(path.join(__dirname, 'public', 'templates', 'login.html'));
-});
+// app.get('/homefarmer', (req, res) => {
+//     if (req.session.user) {
+//         res.sendFile(path.join(__dirname, 'public', 'templates', 'Homefarmer.html'));
+//     }
+//     else res.sendFile(path.join(__dirname, 'public', 'templates', 'login.html'));
+// });
 
 
 
